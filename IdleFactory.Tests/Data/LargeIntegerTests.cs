d281aeb1bd717273166fb3b17047eb147a143acf
Panzareon <fogtociag@web.de>
2025-12-12 21:46:34 +0100

    Add "LargeInteger" class, to allow storing ridiculous amounts of resources

---
+++ IdleFactory.Tests/Data/LargeIntegerTests.cs
@@ -0,0 +1,51 @@
+﻿using IdleFactory.Data;
+
+namespace IdleFactory.Tests.Data
+{
+  internal class LargeIntegerTests
+  {
+    [Test]
+    public void AddLargeIntegerTests()
+    {
+      var value1 = new LargeInteger(1234, 5);
+      var value2 = new LargeInteger(1234, 4);
+      var expectedResult = new LargeInteger(1357, 5);
+
+      Assert.That(value1 + value2, Is.EqualTo(expectedResult));
+      Assert.That(value2 + value1, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void AddAboveMaxLong()
+    {
+      LargeInteger value1 = ulong.MaxValue;
+      LargeInteger value2 = ulong.MaxValue;
+      var expectedResult = new LargeInteger(368_934_881_474_191_032, 2);
+
+      Assert.That(value1 + value2, Is.EqualTo(expectedResult));
+      Assert.That(value2 + value1, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void MultiplyTest()
+    {
+      var value1 = new LargeInteger(100_000_000_000_000_000, 15);
+      var value2 = new LargeInteger(100_000_000_000_000_000, 10);
+
+      var expectedResult = new LargeInteger(1_000_000_000_000_000_000, 41);
+      Assert.That(value1 * value2, Is.EqualTo(expectedResult));
+      Assert.That(value2 * value1, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void MultiplyRandomValuesTest()
+    {
+      var value1 = new LargeInteger(258_942_578_434_345_463, 15);
+      var value2 = new LargeInteger(234_456_789_675_534_244, 15);
+
+      var expectedResult = new LargeInteger(60_710_845_373_262_042, 48);
+      Assert.That(value1 * value2, Is.EqualTo(expectedResult));
+      Assert.That(value2 * value1, Is.EqualTo(expectedResult));
+    }
+  }
+}
---
+++ IdleFactory/Data/LargeInteger.cs
@@ -0,0 +1,225 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace IdleFactory.Data
+{
+  /// <summary>
+  /// An data type that allows handling extremely large integer values, without calculating them too accurately (loosing some precisicion).
+  /// </summary>
+  /// <remarks>
+  /// Basically stores the value as <see cref="BaseValue"/> * <see cref="Base"/> ^ <see cref="Exponent"/>.
+  /// </remarks>
+  public struct LargeInteger(ulong baseValue, long exponent)
+  {
+    /// <summary>
+    /// Not sure if this should use base 10 (for easier display), or base 2 (for easier calculations).
+    /// </summary>
+    public const int Base = 10;
+
+    /// <summary>
+    /// Stores what the maximum exponent for the <see cref="Base"/> would be, that can still be stored in a <see cref="ulong"/>.
+    /// </summary>
+    public static readonly int MaxExponentInLong;
+
+    /// <summary>
+    /// Stores powers of <see cref="Base"/>, with the index being the exponent, from 0 to <see cref="MaxExponentInLong"/>.
+    /// </summary>
+    public static readonly ulong[] ExponentValues;
+
+    /// <summary>
+    /// Initializes some values used for the calculations
+    /// </summary>
+		static LargeInteger()
+    {
+      MaxExponentInLong = (int)MathF.Log(ulong.MaxValue, Base);
+      ExponentValues = new ulong[MaxExponentInLong + 1];
+      ulong value = 1;
+      ExponentValues[0] = value;
+      for (var i = 1; i <= MaxExponentInLong; i++)
+      {
+        value *= Base;
+        ExponentValues[i] = value;
+      }
+    }
+
+    public ulong BaseValue { get; } = baseValue;
+
+    /// <summary>
+    /// Gets or sets the exponent 
+    /// </summary>
+    public long Exponent { get; } = exponent;
+
+    public override readonly string ToString()
+    {
+      if (this.Exponent > 0)
+      {
+        var targetExponent = MaxExponentInLong;
+        for (var i = 0; i <= MaxExponentInLong; i++)
+        {
+          if (this.BaseValue < ExponentValues[i])
+          {
+            targetExponent = i - 1;
+          }
+        }
+
+        var value = this.BaseValue.ToString();
+        return $"{value.Substring(0, 1)}.{value.Substring(1)}E{this.Exponent + targetExponent}";
+      }
+
+      return this.BaseValue.ToString();
+    }
+
+    /// <summary>
+    /// Checks if the other <paramref name="obj"/> is a <see cref="LargeInteger"/> with the same value
+    /// (currently doesn't consider values with same value but different accuracy).
+    /// </summary>
+    /// <param name="obj">The other object to check against.</param>
+    /// <returns>True, if both objects are equal.</returns>
+		public override readonly bool Equals([NotNullWhen(true)] object? obj)
+    {
+      if (obj is not LargeInteger largeInteger)
+      {
+        return false;
+      }
+
+      if (largeInteger.BaseValue == this.BaseValue && largeInteger.Exponent == this.Exponent)
+      {
+        return true;
+      }
+
+      return false;
+    }
+
+    public static implicit operator LargeInteger(ulong value)
+    {
+      return new LargeInteger(value, 0).EnsureBelowLimit();
+    }
+
+    public static LargeInteger operator +(LargeInteger left, LargeInteger right)
+    {
+      if (left.Exponent < right.Exponent)
+      {
+        return Add(right, left);
+      }
+
+      return Add(left, right);
+
+      static LargeInteger Add(LargeInteger left, LargeInteger right)
+      {
+        if (left.Exponent > right.Exponent + MaxExponentInLong)
+        {
+          // right value is too small to add to the left, so result is roughly the same as left
+          return left;
+        }
+
+        right = right.SetExponent(left.Exponent);
+        return new LargeInteger(left.BaseValue + right.BaseValue, left.Exponent).EnsureBelowLimit();
+      }
+    }
+
+    public static LargeInteger operator *(LargeInteger left, LargeInteger right)
+    {
+      return new LargeInteger(left.BaseValue, left.Exponent + right.Exponent) * right.BaseValue;
+    }
+
+    public static LargeInteger operator *(LargeInteger left, ulong right)
+    {
+      if (right == 0)
+      {
+        return new LargeInteger(0, 0);
+      }
+
+      var maxTarget = MaxExponentInLong / 2;
+      int maxExponent = 0;
+
+      // Check if this can be multiplied without reducing accuracy of one side
+      for (var i = 1; i <= maxTarget; i++)
+      {
+        var currentTarget = ExponentValues[i];
+        if (currentTarget > right)
+        {
+          maxExponent = MaxExponentInLong - i;
+          left = left.ReduceAccuracy(maxExponent);
+
+          return new LargeInteger(left.BaseValue * right, left.Exponent);
+        }
+
+        if (currentTarget > left.BaseValue)
+        {
+          maxExponent = MaxExponentInLong - i;
+          return ReduceAccuracyAndMultiply(left, right, maxExponent);
+        }
+      }
+
+      // Both values (without the exponent) are too big, to allow multiplying them without loosing
+      left = left.ReduceAccuracy(maxTarget);
+      return ReduceAccuracyAndMultiply(left, right, maxTarget);
+
+      static LargeInteger ReduceAccuracyAndMultiply(LargeInteger left, ulong right, int maxExponent)
+      {
+        var targetValue = ExponentValues[maxExponent];
+        var exponent = left.Exponent;
+        while (right > targetValue)
+        {
+          right /= Base;
+          exponent++;
+        }
+
+        return new LargeInteger(left.BaseValue * right, exponent);
+      }
+    }
+
+    /// <summary>
+    /// Ensures that the value is small enough, that simple operations can be used without causing an overflow.
+    /// </summary>
+    /// <returns>A copy of this with reduced accuracy, if the <see cref="BaseValue"/> is too big.</returns>
+    private readonly LargeInteger EnsureBelowLimit()
+    {
+      if (this.BaseValue > ExponentValues[MaxExponentInLong - 1])
+      {
+        return this.ReduceAccuracy();
+      }
+
+      return this;
+    }
+
+    private readonly LargeInteger ReduceAccuracy()
+    {
+      return new LargeInteger(this.BaseValue / Base, this.Exponent + 1);
+    }
+
+    private readonly LargeInteger ReduceAccuracy(int maxExponent)
+    {
+      var targetValue = ExponentValues[maxExponent];
+      var current = this;
+      while (current.BaseValue > targetValue)
+      {
+        current = current.ReduceAccuracy();
+      }
+
+      return current;
+    }
+
+    private readonly LargeInteger SetExponent(long targetExponent)
+    {
+      var multiplier = ExponentValues[targetExponent - this.Exponent];
+      return new LargeInteger(this.BaseValue / multiplier, targetExponent);
+    }
+
+    public override readonly int GetHashCode()
+    {
+      return HashCode.Combine(this.BaseValue, this.Exponent);
+    }
+
+    public static bool operator ==(LargeInteger left, LargeInteger right)
+    {
+      return left.Equals(right);
+    }
+
+    public static bool operator !=(LargeInteger left, LargeInteger right)
+    {
+      return !(left == right);
+    }
+  }
+}
